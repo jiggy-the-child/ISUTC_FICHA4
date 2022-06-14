0948f1790497152e25b02763472aead2c1c73c18
jiggy-the-child <denilson.noa@dotcom.co.mz>
2022-06-14 12:18:55 +0200

    solved the number 4 exercise.

--- exercicio4.py
+++ exercicio4.py
@@ -1,23 +1,31 @@
+import random
+
 count = 0
 lista_peca = []
+total_discount = 0
+quantidade = 0
 
 while count < 3:
     count +=1
     nome_peca = str(input('Nome da peça: '))
-    
     if nome_peca.lower().__contains__('paraf') or nome_peca.lower().__contains__('porca') or nome_peca.lower().__contains__('arrue'):
         preco_peca = float(input('Preço da peça: '))
 
         if nome_peca.lower().__contains__('paraf'):
-            preco_peca -= (preco_peca * 10) / 100
+            prod_discount = (preco_peca * 10) / 100
+            preco_peca -= prod_discount
             print(f'O preço desta peça menos 10% de desconto dá: {preco_peca}!')
+            total_discount += prod_discount
         elif nome_peca.lower().__contains__('porca'):
-            preco_peca -= (preco_peca * 20) / 100
+            prod_discount = (preco_peca * 20) / 100
+            preco_peca -= prod_discount
             print(f'O preço desta peça menos 20% de desconto dá: {preco_peca}!')
+            total_discount += prod_discount
         else:
-            preco_peca -= (preco_peca * 30) / 100
+            prod_discount = (preco_peca * 30) / 100
+            preco_peca -= prod_discount
             print(f'O preço desta peça menos 30% de desconto dá: {preco_peca}!')
-        
+            total_discount += prod_discount
         lista_peca.append({
             'nome_peca': nome_peca.title(),
             'preco_peca': preco_peca,
@@ -30,3 +38,20 @@ while count < 3:
                 'nome_peca': nome_peca.title(),
                 'preco_peca': preco_peca,
             })
+
+count_parafuso = 0
+count_porca = 0
+count_arruelas = 0
+client_name = str(input('Introduza o nome do cliente: '))
+for _ in range(random.randint(0, 13)):
+    # when I get here I really get tired!
+    nome = lista_peca[random.randint(0, 2)].get('nome_peca')
+    print(f'{nome}')
+    if nome == 'Parafuso' or nome == 'Parafusos':
+        count_parafuso += 1
+    elif nome == 'Porca' or nome == 'Porcas':
+        count_porca += 1
+    elif nome == 'Arruelas' or nome == 'Arruela':
+        count_arruelas += 1
+
+    print(f'{nome}')
